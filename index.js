0b62b5e4e51ff5e8bd1e76b793c533d0672141c9
Sutimar <macbookpro@Macbooks-MacBook-Pro.local>
2020-07-03 00:22:29 +0700

    3rd commit

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const app = express()
 const PORT = process.env.PORT || 3000
 
 app.get('/', (req,res) => {
-    res.send('test node heroku is OK !!');
+    res.json('test node heroku is OK !!');
 })
 
 app.listen(PORT, () => {
